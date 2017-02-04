4341fdf54102634e674fa8efb1d6245f67ac56c1
Arun Kumar <aruniitkgp7@gmail.com>
2017-02-04 21:26:36 +0530

    Finding a single number among repeated twice nums in an array

--- BitsTwoPointers/BitsTwoPointers/main.c
+++ BitsTwoPointers/BitsTwoPointers/main.c
@@ -18,6 +18,28 @@ int numSetBits(unsigned int A) {
     return count;
 }
 
+/*
+ XOR will returns 1, only on two different bits. So if two numbers are the same, XOR will return 0.
+ 
+ Finally, there is only one number left.
+ 
+ A ^ A = 0 and A ^ B ^ A = B.
+ 
+ So, all even occurences will cancel out using XOR.
+ */
+int singleNumber1(int *A, int n) {
+    
+    int count = 0, i = 0;
+    while (i < n) {
+        count = count ^ A[i];
+        i ++;
+    }
+    return count;
+}
+
 int main(int argc, const char * argv[]) {
+    
+    int A[] = {1,2,2,3,3,8,1};
+    printf("Count : %d\n",singleNumber1(A, sizeof(A)/sizeof(A[0])));
     return 0;
 }
